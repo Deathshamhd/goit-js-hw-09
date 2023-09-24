e7bf104f04f4497ee6e191aada6cd78100f3a71e
Deathshamhd <122151628+Deathshamhd@users.noreply.github.com>
2023-09-24 08:49:20 +0300

    save

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,24 @@
-
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
+  
+  const startBtn = document.querySelector("[data-start]");
+  const stopBtn = document.querySelector("[data-stop]");
+  
+  let timerID = null;
+  
+  stopBtn.disabled = true;
+  
+  startBtn.addEventListener('click', ()=>{
+      timerID = setInterval(()=>{
+          document.body.style.backgroundColor = getRandomHexColor();
+          startBtn.disabled = true;
+          stopBtn.disabled = false;
+      },1000)
+  });
+  
+  stopBtn.addEventListener('click', ()=>{
+      clearInterval(timerID);
+      startBtn.disabled = false;
+      stopBtn.disabled = true;
+  })
